b90ff3e3aab3e6326b0fc9225a74b207323ddcf0
mzk <amiable827@gmail.com>
2025-09-30 21:26:04 +0900

    純アルコール量の分

--- alcohol.py
+++ alcohol.py
@@ -1,8 +1,10 @@
 before = 25 # 割る前のアルコール濃度（％）
-amount = 350 # 割った後のお酒の量（ml）
+amount = 1000 # 割った後のお酒の量（ml）
 after = 10 # 割った後のアルコール濃度（％）
 
 # アルコールの素の量を計算
 alcohol = amount * after / before
+pure = alcohol * before / 100
 
-print(alcohol)
+print(f"{alcohol} ml")
+print(f"{pure} ml")
