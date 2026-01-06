f7e11888935edd1f47c55796f9070dd90f6842fd
mzk <amiable827@gmail.com>
2026-01-07 01:07:14 +0900

    連番画像から動画作成機能

--- multi.py
+++ multi.py
@@ -3,10 +3,13 @@
 
 import os
 import sys
+import time
+import glob
+import tempfile
+import cv2
 import vlc
 import yt_dlp
 import numpy as np
-import cv2
 from superqt import QRangeSlider
 from PySide6.QtWidgets import (
     QApplication,
@@ -67,15 +70,19 @@ class VideoPlayer(QMainWindow):
         super().__init__()
         self.setWindowTitle("PySide6 VLC Player")
         self.resize(1000, 700)
+        self.media = None
         self.send_filename = None
+        self.tmp = None
 
         self.dl_window = DownloadWindow()
         self.dl_window.hide()
         self.dl_window.video.connect(self.open_file)
         self.make_seq = MakeSeqWindow(video_path=None)
         self.make_seq.hide()
-        self.make_video = MakeVideo()
+        self.make_video = MakeVideoWindow()
         self.make_video.hide()
+        self.make_video.temp_video.connect(self.temp_video_process)
+        self.make_video.saved_video.connect(self.open_file)
 
         # VLC初期化
         self.instance = vlc.Instance()
@@ -100,7 +107,6 @@ class VideoPlayer(QMainWindow):
         self.video_to_seq.setDisabled(True)
         self.video_to_seq.triggered.connect(self.open_make_seq_window)
         self.seq_to_video = file.addAction("Convert sequential images to video")
-        self.seq_to_video.setDisabled(True)
         self.seq_to_video.triggered.connect(lambda: self.make_video.show())
 
         dl = menu_bar.addMenu("yt_dlp")
@@ -188,7 +194,7 @@ class VideoPlayer(QMainWindow):
 
     def toggle_play(self):
         state = self.player.get_state()
-        if state == vlc.State.NothingSpecial:
+        if not self.media:
             self.open_file(fn=None)
         elif state == vlc.State.Playing:
             self.player.pause()
@@ -252,7 +258,26 @@ class VideoPlayer(QMainWindow):
         self.make_seq = MakeSeqWindow(self.send_filename)
         self.make_seq.show()
 
+    def temp_video_process(self, path):
+        self.clean_temp_file()
+        self.tmp = path
+        self.open_file(fn=self.tmp)
+
+    def clean_temp_file(self):
+        if self.player:
+            self.player.stop()
+            self.player.set_media(None)
+        if self.tmp and os.path.exists(self.tmp):
+            try:
+                os.remove(self.tmp)
+                print(f"{self.tmp} is exist: {os.path.exists(self.tmp)}")
+            except PermissionError as e:
+                print(f"Failed to Delete TempFile: {e}")
+            self.tmp = None
+
     def closeEvent(self, event):
+        print(self.tmp)
+        self.clean_temp_file()
         self.dl_window.close()
         self.make_seq.close()
         self.make_video.close()
@@ -621,7 +646,7 @@ class MakeSeqWindow(QDialog):
 
     def make_seq_images(self):
         if self.radio_all.isChecked():
-            start = 0
+            start = 1
             end = self.total_frames
         elif self.radio_custam.isChecked():
             start = self.input_start.value()
@@ -634,7 +659,7 @@ class MakeSeqWindow(QDialog):
         ext = self.ext_box.currentText()
 
         def show_progress(value):
-            self.proc_status.setText(f"{value} / {end - start + 1}")
+            self.proc_status.setText(f"{int(value)} / {int(end - start + 1)}")
 
         def finished():
             QMessageBox.information(self, "Finish", "Finished Makeing Sequential Images")
@@ -698,11 +723,225 @@ class MakeSeqWorker(QObject):
         self.finished.emit()
 
 
-class MakeVideo(QDialog):
+class MakeVideoWindow(QDialog):
+    temp_video = Signal(str)
+    saved_video = Signal(str)
+
     def __init__(self):
         super().__init__()
         self.resize(600, 400)
         self.setWindowTitle("Make Video From Sequential Images")
+        self.folder_path = USER_DOWNLOAD_FOLDER
+
+        layout = QVBoxLayout(self)
+
+        # パス
+        self.path_layout = QVBoxLayout()
+        self.image_path_layout = QHBoxLayout()
+
+        self.exp_label = QLabel("Supports only the format\n filename_0000.jpg, .png, .bmp")
+        self.exp_label.setAlignment(Qt.AlignCenter)
+        self.image_path = QLineEdit()
+        self.image_path.setReadOnly(True)
+        self.image_folder_button = QPushButton("select")
+        self.image_folder_button.clicked.connect(self.select_image_folder)
+
+
+        self.path_layout.addWidget(self.exp_label)
+        self.path_layout.addSpacing(20)
+        self.image_path_layout.addWidget(QLabel("Image Path"))
+        self.image_path_layout.addSpacing(20)
+        self.image_path_layout.addWidget(self.image_path)
+        self.image_path_layout.addStretch()
+        self.image_path_layout.addWidget(self.image_folder_button)
+        self.path_layout.addLayout(self.image_path_layout)
+
+        # 生成形式
+        self.radio_layout = QVBoxLayout()
+
+        self.radio_play_only = QRadioButton("Only Play")
+        self.radio_save_only = QRadioButton("Only Save")
+        self.radio_save_and_play = QRadioButton("Save and Play")
+        self.radio_save_and_play.setChecked(True)
+
+        self.radio_layout.addWidget(self.radio_play_only)
+        self.radio_layout.addWidget(self.radio_save_only)
+        self.radio_layout.addWidget(self.radio_save_and_play)
+
+        # 入力
+        self.input_layout = QVBoxLayout()
+        self.folder_layout = QHBoxLayout()
+        self.filename_layout = QHBoxLayout()
+
+        self.input_folder_path = QLineEdit(self.folder_path)
+        self.input_folder_path.setReadOnly(True)
+        self.select_button = QPushButton("select")
+        self.select_button.clicked.connect(self.select_save_folder)
+        self.save_filename_input = QLineEdit()
+
+        self.folder_layout.addWidget(QLabel("Save Path"))
+        self.folder_layout.addSpacing(20)
+        self.folder_layout.addWidget(self.input_folder_path)
+        self.folder_layout.addStretch()
+        self.folder_layout.addWidget(self.select_button)
+
+        self.filename_layout.addWidget(QLabel("Save Filename"))
+        self.filename_layout.addSpacing(20)
+        self.filename_layout.addWidget(self.save_filename_input)
+        self.filename_layout.addStretch()
+    
+        self.input_layout.addLayout(self.folder_layout)
+        self.input_layout.addLayout(self.filename_layout)
+
+        # fps
+        self.fps_layout = QHBoxLayout()
+        
+        self.fps = QSpinBox()
+        self.fps.setMinimum(1)
+        self.fps.setValue(30)
+
+        self.fps_layout.addWidget(QLabel("fps"))
+        self.fps_layout.addStretch()
+        self.fps_layout.addWidget(self.fps)
+        self.fps_layout.addStretch()
+
+        # 変換状況
+        self.status_layout = QVBoxLayout()
+
+        self.status_label = QLabel("Conversion Processing Status")
+        self.status_label.setAlignment(Qt.AlignCenter)
+        self.process_label = QLabel("0 / 0")
+        self.process_label.setAlignment(Qt.AlignCenter)
+        self.status_layout.addWidget(self.status_label)
+        self.status_layout.addWidget(self.process_label)
+
+        # スタートボタン
+        self.start_button = QPushButton("start")
+        self.start_button.clicked.connect(self.make_video)
+
+        layout.addLayout(self.path_layout)
+        layout.addStretch()
+        layout.addLayout(self.radio_layout)
+        layout.addStretch()
+        layout.addLayout(self.input_layout)
+        layout.addLayout(self.fps_layout)
+        layout.addStretch()
+        layout.addLayout(self.status_layout)
+        layout.addWidget(self.start_button)
+
+    def select_save_folder(self):
+        foldername = QFileDialog.getExistingDirectory(self, "select folder")
+        if foldername:
+            self.folder_path = foldername
+            self.input_folder_path.setText(foldername)
+
+    def select_image_folder(self):
+        foldername = QFileDialog.getExistingDirectory(self, "select folder")
+        if foldername:
+            self.image_path.setText(foldername)
+
+    def make_video(self):
+        def get_images():
+            files = []
+            extension = ("jpg", "png", "bmp")
+            for ext in extension:
+                files.append(glob.glob(f"{self.image_path.text()}/*_[0-9][0-9][0-9][0-9].{ext}"))
+            return files
+
+        def sort_file(file):
+            sorted_file = sorted(file, key=lambda f: int(os.path.splitext(f)[0][-4:]))
+            return sorted_file
+        
+        def get_size(file):
+            img = cv2.imread(file[0])
+            h, w, _ = img.shape
+            return w, h
+        
+        def progress_status(value):
+            self.process_label.setText(f"{value} / {len(file)}")
+        
+        def finished():
+            QMessageBox.information(self, "Finish", "Finish Making Video")
+            self.make_video_thread.deleteLater()
+            if self.radio_save_and_play.isChecked():
+                self.saved_video.emit(save_path)
+            elif self.radio_play_only.isChecked():
+                self.temp_video.emit(self.tmp.name)
+            self.close()
+
+        if self.image_path.text() == "":
+            QMessageBox.warning(self, "Path error", "Please Input Image Path")
+            return
+        if self.radio_save_and_play.isChecked() and self.save_filename_input.text() == "":
+            QMessageBox.warning(self, "Filename error", "Please Input Filename")
+            return
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        files = get_images()
+        """
+        今はいったん１つの形式にのみ対応
+        今後プレイリスト形式にしたときに複数形式や同一形式複数ファイルに対応予定
+        """
+        if not files[0] and not files[1] and not files[2]:
+            print("No Images")
+            return
+        elif files[0]:
+            file = files[0]
+        elif files[1]:
+            file = files[1]
+        elif files[2]:
+            file = files[2]
+
+        file = sort_file(file)
+        width, height = get_size(file)
+        if self.radio_play_only.isChecked():
+            self.tmp = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
+            self.tmp.close()
+            save_path = self.tmp.name
+        elif self.radio_save_only.isChecked() or self.radio_save_and_play.isChecked():
+            save_path = os.path.join(self.folder_path, self.save_filename_input.text() + ".mp4")
+            os.makedirs(self.folder_path, exist_ok=True)
+        fps = self.fps.value()
+
+        self.make_video_worker = MakeVideoWorker(file, save_path, fourcc, fps, width, height)
+        self.make_video_thread = QThread()
+        self.make_video_worker.moveToThread(self.make_video_thread)
+
+        self.make_video_thread.started.connect(self.make_video_worker.run)
+        self.make_video_worker.progress.connect(progress_status)
+        self.make_video_worker.finished.connect(self.make_video_thread.quit)
+        self.make_video_worker.finished.connect(self.make_video_worker.deleteLater)
+        self.make_video_thread.finished.connect(finished)
+
+        self.make_video_thread.start()
+
+
+class MakeVideoWorker(QObject):
+    finished = Signal()
+    progress = Signal(int)
+
+    def __init__(self, file, filename, fourcc, fps, width, height):
+        super().__init__()
+        self.file = file
+        self.filename = filename
+        self.fourcc = fourcc
+        self.fps = fps
+        self.size = (width, height)
+
+    @Slot()
+    def run(self):
+        self.video = cv2.VideoWriter(self.filename, self.fourcc, self.fps, self.size)
+        i = 1
+        try:
+            for f in self.file:
+                img = cv2.imread(f)
+                if img is None:
+                    continue
+                self.video.write(img)
+                self.progress.emit(i)
+                i += 1
+        finally:
+            self.video.release()
+            self.finished.emit()
 
 
 if __name__ == "__main__":
