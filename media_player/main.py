0190de2100daa401672043ece0c752399ead35fb
mzk <amiable827@gmail.com>
2026-01-15 22:57:52 +0900

    exe化のための編集

--- media_player/main.py
+++ media_player/main.py
@@ -1,6 +1,12 @@
+import os
 import sys
 from PySide6.QtWidgets import QApplication
 from player import VideoPlayer
+
+base_dir = getattr(sys, "_MEIPASS", os.path.dirname(__file__))
+vlc_dir = os.path.join(base_dir, "vlc")
+
+os.environ["VLC_PLUGIN_PATH"] = os.path.join(vlc_dir, "plugins")
 
 def main():
     app = QApplication(sys.argv)
