34394b351534a490fb4dd7117ba347547e058da8
mzk <amiable827@gmail.com>
2026-06-09 19:54:22 +0900

    マージソート実装

--- sort/main.py
+++ sort/main.py
@@ -2,6 +2,7 @@ import sys
 import random
 
 from bubble import bubble_sort
+from merge import merged_history
 
 from PySide6.QtWidgets import (
     QApplication,
@@ -27,24 +28,32 @@ from PySide6.QtCore import (
     QTimer,
 )
 
+BLOCK_SIZE = 10
+ARRAY_SIZE = 100
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.resize(1000, 1000)
+        self.resize(BLOCK_SIZE * ARRAY_SIZE + 50, BLOCK_SIZE * ARRAY_SIZE + 50)
         self.setStyleSheet("background-color: black")
         self.scene = QGraphicsScene()
         view = QGraphicsView()
         view.setScene(self.scene)
         self.setCentralWidget(view)
 
-        self.array_size = 31
+        self.array_size = ARRAY_SIZE + 1
         self.interval = 50
+        self.bars = []
+
         self.get_bubble_index = False
+
+        self.merge_index = 0
+        self.merge_history = []
 
         self.dialog = DialogWindow()
         self.dialog.shfl.connect(self.shuffle_array)
         self.dialog.bbl.connect(self.start_bubble_timer)
+        self.dialog.mrg.connect(self.start_merge_timer)
         self.dialog.bg.connect(self.start_bogo_timer)
         self.dialog.stp.connect(self.stop_timer)
         self.dialog.show()
@@ -52,28 +61,38 @@ class MainWindow(QMainWindow):
         self.array = [i for i in range(1, self.array_size)]
         random.shuffle(self.array)
 
-        self.bars = []
-        for i in range(self.array_size - 1):
-            bar = Bar(i, self.array[i])
-            self.bars.append(bar)
-            self.scene.addItem(bar)
+        self.set_bars()
 
         self.bubble_timer = QTimer(self)
         self.bubble_timer.timeout.connect(self.bbl_sort)
+        self.merge_timer = QTimer(self)
+        self.merge_timer.timeout.connect(self.merge_sort)
         self.bogo_timer = QTimer(self)
         self.bogo_timer.timeout.connect(self.check_bogo_sort)
     
-    def shuffle_array(self):
-        self.scene.clear()
-        self.get_bubble_index = False
-        random.shuffle(self.array)
+    def set_bars(self):
         self.bars = []
         for i in range(self.array_size - 1):
             bar = Bar(i, self.array[i])
             self.bars.append(bar)
             self.scene.addItem(bar)
+    
+    # 再配置
+    def shuffle_array(self):
+        self.scene.clear()
+
+        # バブルソート初期化
+        self.get_bubble_index = False
+
+        #マージソート初期化
+        self.merge_index = 0
+        self.merge_history = []
+    
+        random.shuffle(self.array)
+        self.set_bars()
         self.scene.update()
 
+    # ここからバブルソート
     def start_bubble_timer(self):
         self.set_disabled()
         self.bubble_timer.start(self.interval)
@@ -106,6 +125,33 @@ class MainWindow(QMainWindow):
         self.bars[a], self.bars[b] = self.bars[b], self.bars[a]
         self.scene.update()
 
+    # ここからマージソート
+    def start_merge_timer(self):
+        self.set_disabled()
+        self.merge_timer.start(100)
+
+    def merge_sort(self):
+        if self.merge_history == []:
+            self.merge_history = merged_history(self.array)
+
+        if self.merge_index < len(self.merge_history):
+            self.update_bar(self.merge_history[self.merge_index])
+            self.scene.update()
+            self.merge_index += 1
+        else:
+            self.merge_timer.stop()
+            QMessageBox(self, "Finish", "ソート完了")
+            self.set_enabled()
+
+    def update_bar(self, array):
+        self.scene.clear()
+        self.bars = []
+        for i, a in enumerate(array):
+            bar = Bar(i, a)
+            self.bars.append(bar)
+            self.scene.addItem(bar)
+
+    # ここからボゴソート
     def start_bogo_timer(self):
         self.get_bubble_index = False
         self.set_disabled()
@@ -120,21 +166,28 @@ class MainWindow(QMainWindow):
             self.set_enabled()
             QMessageBox.information(self, "miracle", "ソート完了")
 
+    # タイマー停止
     def stop_timer(self):
         if self.bubble_timer.isActive():
             self.bubble_timer.stop()
+        if self.merge_timer.isActive():
+            self.merge_timer.stop()
         if self.bogo_timer.isActive():
             self.bogo_timer.stop()
         self.set_enabled()
 
+    # ボタンを押下可能に
     def set_enabled(self):
         self.dialog.shuffle_button.setEnabled(True)
         self.dialog.bubble_button.setEnabled(True)
+        self.dialog.merge_button.setEnabled(True)
         self.dialog.bogo_button.setEnabled(True)
 
+    # 不可能に
     def set_disabled(self):
         self.dialog.shuffle_button.setEnabled(False)
         self.dialog.bubble_button.setEnabled(False)
+        self.dialog.merge_button.setEnabled(False)
         self.dialog.bogo_button.setEnabled(False)
 
     def keyPressEvent(self, event):
@@ -154,7 +207,7 @@ class Bar(QGraphicsRectItem):
         super().__init__()
         self.num = num
         self.val = val
-        self.block = 30
+        self.block = BLOCK_SIZE
 
         self.setRect(
             0, 0,
@@ -171,6 +224,7 @@ class Bar(QGraphicsRectItem):
 class DialogWindow(QDialog):
     shfl = Signal()
     bbl = Signal()
+    mrg = Signal()
     bg = Signal()
     stp = Signal()
 
@@ -188,6 +242,10 @@ class DialogWindow(QDialog):
         self.bubble_button = QPushButton("バブルソート")
         self.bubble_button.clicked.connect(self.bubble)
         layout.addWidget(self.bubble_button)
+
+        self.merge_button = QPushButton("マージソート")
+        self.merge_button.clicked.connect(self.mrg)
+        layout.addWidget(self.merge_button)
 
         self.bogo_button = QPushButton("ボゴソート")
         self.bogo_button.clicked.connect(self.bogo)
---
+++ sort/merge.py
@@ -0,0 +1,53 @@
+base = []
+history = []
+
+def merge_sort(a):
+    if len(a) <= 1:
+        return a
+    
+    array = a
+
+    split = len(array) // 2
+
+    left = merge_sort(array[:split])
+    right = merge_sort(array[split:])
+
+    merged_list = merge(left, right)
+    index_history(left, right, merged_list)
+
+    return merged_list
+
+def merge(l, r):
+    result = []
+    i, j = 0, 0
+
+    while(i < len(l)) and (j < len(r)):
+        if l[i] <= r[j]:
+            result.append(l[i])
+            i += 1
+        else:
+            result.append(r[j])
+            j += 1
+
+    if i < len(l):
+        result.extend(l[i:])
+    if j < len(r):
+        result.extend(r[j:])
+    return result
+
+def index_history(left, right, merged_list):
+    start = base.index(left[0])
+    end = start + len(left) + len(right)
+    base[start:end] = merged_list
+
+    history.append(base.copy())
+
+# main関数
+def merged_history(array):
+    global history
+    global base
+    base = array.copy()
+    history = []
+    _ = merge_sort(array)
+
+    return history
